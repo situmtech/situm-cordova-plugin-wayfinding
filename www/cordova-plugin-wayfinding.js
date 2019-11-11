2a2af3b64c1112c320bcc1bc590b6ffb322b3b95
Fernando Sanchez Vilas <fsvilas@gmail.com>
2019-11-11 08:32:24 +0100

    Added js bridge

--- www/cordova-plugin-wayfinding.js
+++ www/cordova-plugin-wayfinding.js
@@ -1,5 +1,33 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cordova-plugin-wayfinding', 'coolMethod', [arg0]);
+// Empty constructor
+function SitumWayfindingPlugin() {}
+
+SitumWayfindingPlugin.prototype.load = function (map_div, success, error) {
+  // Create a Google Maps native view under the map_canvas div.
+  var map = plugin.google.maps.Map.getMap(map_div);
+  map.on(plugin.google.maps.event.MAP_READY, function(latLng) {
+        exec(success,  //success callback
+        error,                //error callback
+        "SitumWayfindingPlugin",           //class name
+        "load",      //action name
+        []);        //args
+  });
+  return map;
 };
+
+SitumWayfindingPlugin.prototype.unload = function (map, success, error) {
+  map.remove();
+};
+
+
+// Installation constructor that binds SitumWayfindingPlugin to window
+SitumWayfindingPlugin.install = function() {
+  if (!plugin.situm.wayfinding) {
+    plugin.situm.wayfinding = {};
+  }
+  plugin.situm.wayfinding.situmWayfindingPlugin = new SitumWayfindingPlugin();
+  return plugin.situm.wayfinding.situmWayfindingPlugin;
+};
+
+cordova.addConstructor(SitumWayfindingPlugin.install);
